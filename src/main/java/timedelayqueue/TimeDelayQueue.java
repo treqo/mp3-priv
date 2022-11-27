b8db27f1acf80e3ea7ac58df11df4a13d66ef0d8
Tareq Alansari <tareq.ayz@gmail.com>
2022-11-26 22:34:26 -0800

    task1 done a bit more

--- src/main/java/timedelayqueue/TimeDelayQueue.java
+++ src/main/java/timedelayqueue/TimeDelayQueue.java
@@ -10,13 +10,47 @@ import java.util.UUID;
 // TODO: State the rep invariant and abstraction function
 // TODO: what is the thread safety argument?
 
+class MsgThread extends Thread {
+
+    private final long start;
+    private final long end;
+
+    public MsgThread() {
+        start = System.currentTimeMillis();
+        end = -1; //change later
+    }
+
+    public MsgThread(int msgLifetime) {
+        long current = System.currentTimeMillis();
+        this.start = current;
+        this.end = current + msgLifetime;
+    }
+
+    @Override
+    public void run()
+    {
+        System.out.println("Thread is running");
+    }
+
+    public long runTime() {
+        return System.currentTimeMillis() - start;
+    }
+
+    public boolean isContinuing() {
+        if(System.currentTimeMillis() < end || end == -1) {
+            return true;
+        }
+        return false;
+    }
+}
+
 public class TimeDelayQueue {
 
     private PriorityQueue<PubSubMessage> pq;
     private int delay;
     private int count = 0;
 
-    Thread msgThread = new Thread();
+    MsgThread msgThread;
 
     // a comparator to sort messages
     private class PubSubMessageComparator implements Comparator<PubSubMessage> {
@@ -32,7 +66,7 @@ public class TimeDelayQueue {
     public TimeDelayQueue(int delay) {
         this.delay = delay;
         this.pq = new PriorityQueue<>(new PubSubMessageComparator());
-        this.msgThread = new Thread();
+        this.msgThread = new MsgThread();
         msgThread.start();
     }
 
@@ -63,8 +97,10 @@ public class TimeDelayQueue {
     // if there is ni suitable message
     public PubSubMessage getNext() {
 
-//        if(msgThread.runTime())
-        return pq.poll();
+        if(msgThread.runTime() >= delay) {
+            return pq.poll();
+        }
+        return PubSubMessage.NO_MSG;
     }
 
     // return the maximum number of operations
